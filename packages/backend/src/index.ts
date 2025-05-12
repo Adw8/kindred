4e8b09ea288ea3dfa64842106481e01289872f50
Adw8 <adwaitgharpure@gmail.com>
2025-05-12 22:31:45 +0530

    Add try catch for server code

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -17,19 +17,28 @@ async function startServer() {
 startServer();
 
 app.get('/api/friends/:userId', async (req: Request, res: Response) => {
-
-  const friends = await getFriendsForUser(req.params.userId);
-  res.json(friends);
+  try {
+    const friends = await getFriendsForUser(req.params.userId);
+    res.json(friends);
+  } catch (error) {
+    console.error('Error fetching friends:', error);
+    res.status(500).json({ error: 'Failed to fetch friends' });
+  }
 });
 
 app.post('/api/add-friend', async (req: Request, res: Response) => {
-  const { userId, name, birthday, info } = req.body;
+  try {
+    const { userId, name, birthday, info } = req.body;
 
-  if (!name || !birthday || !info) {
-    res.status(400).json({ error: 'All fields are required' });
+    if (!name || !birthday || !info) {
+      res.status(400).json({ error: 'All fields are required' });
+    }
+
+    await addFriendToDatabase(userId, name, birthday, info);
+
+    res.status(200).json('Friend added successfully!');
+  } catch (error) {
+    console.error('Error Adding friend to DB:', error);
+    res.status(500).json({ error: 'Error Adding friend to DB' });
   }
-
-  await addFriendToDatabase(userId, name, birthday, info);
-
-  res.status(200).json('Friend added successfully!');
 });
